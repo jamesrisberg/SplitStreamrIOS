85b5d0278222533f400159d11594cbab1be859c2
James Risberg <jimmyrisberg56@gmail.com>
2016-03-20 14:19:27 -0400

    Chunks playing from queue, not entirely smooth

--- SplitStreamr/Constants.swift
+++ SplitStreamr/Constants.swift
@@ -56,9 +56,16 @@ let documentsPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .Use
 
 // MARK: Debug
 
+let dateFormatter: NSDateFormatter = {
+    let formatter = NSDateFormatter()
+    formatter.locale = NSLocale.currentLocale()
+    formatter.dateFormat = "HH:mm:ss.SSS"
+    return formatter
+}()
+
 func debugLog(message: String) {
     dispatch_async(dispatch_get_main_queue(), {
-        print(message);
+        print("\(dateFormatter.stringFromDate(NSDate())) | \(message)");
     });
 }
 
